3490f277a3217efa908affdddbbb95af10710f0b
alameen-coder <alameentaofeek01@gmail.com>
2023-12-16 19:57:20 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -49,8 +49,8 @@ allLinks.forEach(function (link) {
     }
 
     // Close mobile navigation
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");      
   });
 });
 
